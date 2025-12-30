47972949cdd962b58e207d20bff1dc3d6acba453
Usama Bhakrani <usamabhakrani@gmail.com>
2025-12-30 22:31:11 +0500

    upload files functionality

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
-
+from sqlalchemy import select
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -54,3 +54,32 @@ def delete_post(id: int):
         raise HTTPException(status_code=404, detail="Post not found")
     deleted_post = text_posts.pop(id)
     return {"detail": "Post deleted successfully", "post": deleted_post}     
+
+
+@app.post('/upload')
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(...),
+    session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(caption=caption, url='Dummy Url', file_type='photo', file_name='dummy name')
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+
+@app.get('/feed')    
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    posts = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    return [row[0] for row in posts.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append({
+            "id":str(post.id),
+            "caption":post.caption,
+            "url":post.url,
+            "file_type":post.file_type,
+            "filename":post.filename,
+            "created_at":post.created_at.isoformat()
+        })
+    return {"posts": posts_data}    
